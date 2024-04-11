67a458f0c4a171d0a495ac27fd6c3b67e1823300
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-04-11 20:26:28 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Symbol from "./components/Symbol.svelte";
+  import { KATA_MAP, ROMANJI_CHARS } from "./data/kataMap";
+  function randomChar() {
+    return Object.values(ROMANJI_CHARS)[
+      Math.floor(Math.random() * Object.values(ROMANJI_CHARS).length)
+    ];
+  }
+
+  let char = randomChar();
+  let romanji = true;
+  let kataChoice = "hiragana";
+  $: hiragana = kataChoice == "hiragana";
+</script>
+
+<main class="f cs">
+  <h1>Nise no tori</h1>
+  <div class="f f1 cc">
+    <Symbol kata={KATA_MAP[char]} {romanji} {hiragana} katakana={!hiragana} />
+    <div>
+      <button class="mg" on:click={() => (char = randomChar())}>More</button>
+      <form class="f cc">
+        <div class="f rc">
+          <label for="romanji">Romanji</label>
+          <input type="checkbox" name="romanji" bind:checked={romanji} />
+        </div>
+        <div class="f rc">
+          <label for="hiragana">Hiragana</label>
+          <input
+            name="hiragana"
+            type="radio"
+            bind:group={kataChoice}
+            value="hiragana"
+          />
+        </div>
+        <div class="f rc">
+          <label for="katakana">Katakana</label>
+          <input
+            name="katakana"
+            type="radio"
+            bind:group={kataChoice}
+            value="katakana"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/components/Symbol.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let kata = null;
+    export let hiragana = true;
+    export let katakana = false;
+    export let romanji = false;
+</script>
+
+{#if !Boolean(kata)}
+    <div />
+{:else}
+    <div class="symbol f cc">
+        {#if romanji}
+            <span>{kata.romanjiLabel || kata.romanji}</span>
+        {/if}
+        <h1>
+            {#if hiragana}
+                {kata.hiragana}
+            {:else if katakana}
+                {kata.katakana}
+            {:else}
+                {kata.hiragana}
+            {/if}
+        </h1>
+    </div>
+{/if}
+
+<style>
+    div.symbol {
+        border: var(--borders);
+        border-radius: var(--border-radius);
+        padding: 1rem 2rem;
+    }
+
+    h1 {
+        font-size: 3.5rem;
+    }
+</style>
