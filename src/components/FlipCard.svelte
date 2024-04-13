aab23d0d48ec115ca13f0847eaec10abe2ca1927
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-04-13 14:13:50 +0100

    first flipcard implementation

---
+++ src/components/FlipCard.svelte
@@ -0,0 +1,79 @@
+<script>
+    export let kata = null;
+    export let hiragana = true;
+    export let katakana = false;
+    export let flipped = false;
+</script>
+
+<div class="flipCard">
+    <div class="flipCard-inner" class:flipped>
+        <div class="flipCard-front f cc">
+            <h1>
+                {#if hiragana}
+                    {kata.hiragana}
+                {:else if katakana}
+                    {kata.katakana}
+                {:else}
+                    {kata.hiragana}
+                {/if}
+            </h1>
+        </div>
+        <div class="flipCard-back f cc">
+            {#if flipped}
+                <h2>{kata.romanjiLabel || kata.romanji}</h2>
+            {/if}
+        </div>
+    </div>
+</div>
+
+<style>
+    .flipCard {
+        background-color: transparent;
+        width: 150px;
+        height: 190px;
+        perspective: 1000px;
+    }
+
+    .flipCard-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.6s;
+        transform-style: preserve-3d;
+        box-shadow: var(--box-shadow);
+        border-radius: var(--border-radius);
+    }
+
+    .flipped {
+        transform: rotateY(180deg);
+    }
+
+    .flipCard-front,
+    .flipCard-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        border-radius: var(--border-radius);
+    }
+
+    .flipCard-front {
+        color: white;
+        background-color: var(--accent-color);
+    }
+
+    .flipCard-back {
+        background-color: var(--accent-v1-color);
+        color: white;
+        transform: rotateY(180deg);
+    }
+
+    h1 {
+        font-size: 3.2rem;
+    }
+    h2 {
+        font-size: 3rem;
+    }
+</style>
--- src/pages/Cards.svelte
+++ src/pages/Cards.svelte
@@ -1,28 +1,42 @@
 <script>
-  import Symbol from "../components/Symbol.svelte";
+  import FlipCard from "../components/FlipCard.svelte";
   import { HIRAGANA, KATAKANA } from "../libs/data/consts";
   import { randomKata } from "../libs/utils";
 
+  let result = {
+    shown: 1,
+    correct: 0,
+    wrong: 0,
+  };
+
   let kata = randomKata();
-  let romanji = false;
+  let flipped = false;
+  let wasFlipped = false;
   let kataChoice = HIRAGANA;
   $: hiragana = kataChoice == HIRAGANA;
+
+  function correct() {
+    result.correct += 1;
+    next();
+  }
+
+  function wrong() {
+    result.wrong += 1;
+    next();
+  }
+
+  function next() {
+    flipped = false;
+    wasFlipped = false;
+    kata = randomKata();
+    result.shown += 1;
+  }
 </script>
 
-<div class="f f1 cc">
-  <Symbol {kata} {romanji} {hiragana} katakana={!hiragana} />
+<div>
+  <h1>Card Game</h1>
+  <h2>Shown: {result.shown} Correct: {result.correct} Wrong: {result.wrong}</h2>
   <div>
-    <div class="g_5 mg fi">
-      <button class="primary" on:click={() => (romanji = !romanji)}>
-        Show
-      </button>
-      <button
-        on:click={() => {
-          romanji = false;
-          kata = randomKata();
-        }}>Next</button
-      >
-    </div>
     <div class="f rc">
       <label for={HIRAGANA}>Hiragana</label>
       <input
@@ -43,3 +57,35 @@
     </div>
   </div>
 </div>
+<div class="f f1 cc">
+  <FlipCard {kata} {hiragana} katakana={!hiragana} {flipped} />
+</div>
+
+<div class="mg fi">
+  <button
+    class="warning"
+    on:click={() => {
+      if (!wasFlipped) {
+        wasFlipped = true;
+      }
+      flipped = !flipped;
+    }}
+  >
+    Flip
+  </button>
+</div>
+<div class="mg2 g_5">
+  <button class="danger" disabled={!wasFlipped} on:click={wrong}>
+    Wrong
+  </button>
+  <button class="success" disabled={!wasFlipped} on:click={correct}>
+    Correct
+  </button>
+</div>
+
+<style>
+  h1,
+  h2 {
+    text-align: center;
+  }
+</style>
