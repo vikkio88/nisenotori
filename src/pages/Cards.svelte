989edac1daae304afcce0405cf5a78d2ad4bc467
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-04-15 19:41:25 +0100

    reordered componet placement

--- src/pages/Cards.svelte
+++ src/pages/Cards.svelte
@@ -1,56 +1,66 @@
 <script>
-    import Select from "../components/shared/Select.svelte";
-    import CardsMemo from "../games/CardsMemo.svelte";
-    import { CHARSETS, charsetOptions } from "../libs/data/charSets";
-    import { HIRAGANA, KATAKANA } from "../libs/data/consts";
+  import { navigate } from "svelte-routing";
+  import Select from "../components/shared/Select.svelte";
+  import CardsMemo from "../games/CardsMemo.svelte";
+  import { CHARSETS, charsetOptions } from "../libs/data/charSets";
+  import { HIRAGANA, KATAKANA } from "../libs/data/consts";
 
-    let gameStarted = false;
+  let gameStarted = false;
 
-    let kata = HIRAGANA;
-    let selectedCharsetIndex = 0;
+  let kata = HIRAGANA;
+  let selectedCharsetIndex = 0;
+
+  let endlessMode = false;
 </script>
 
 {#if gameStarted}
-    <CardsMemo
-        charset={CHARSETS[selectedCharsetIndex].charset}
-        charsetLabel={CHARSETS[selectedCharsetIndex].label}
-        kataChoice={kata}
-    />
+  <CardsMemo
+    charset={CHARSETS[selectedCharsetIndex].charset}
+    charsetLabel={CHARSETS[selectedCharsetIndex].label}
+    kataChoice={kata}
+    remove={!endlessMode}
+    restart={() => (gameStarted = false)}
+    end={() => navigate("/", { replace: true })}
+  />
 {:else}
-    <h1>Card Memo Game</h1>
-    <div class="f f1 cc g">
-        <div>
-            <h3>Syllabary</h3>
-            <div class="f rc">
-                <label for={HIRAGANA}>Hiragana</label>
-                <input
-                    name={HIRAGANA}
-                    type="radio"
-                    bind:group={kata}
-                    value={HIRAGANA}
-                />
-            </div>
-            <div class="f rc">
-                <label for={KATAKANA}>Katakana</label>
-                <input
-                    name={KATAKANA}
-                    type="radio"
-                    bind:group={kata}
-                    value={KATAKANA}
-                />
-            </div>
-        </div>
-
-        <div>
-            <h3>Charset</h3>
-            <Select
-                options={charsetOptions}
-                bind:selected={selectedCharsetIndex}
-            />
-        </div>
-
-        <button class="big success mg" on:click={() => (gameStarted = true)}>
-            Start
-        </button>
+  <h1>Card Memo Game</h1>
+  <div class="f f1 cc g">
+    <div class="f cc g1">
+      <h3>Syllabary</h3>
+      <div class="f rc">
+        <label for={HIRAGANA}>Hiragana</label>
+        <input
+          name={HIRAGANA}
+          type="radio"
+          bind:group={kata}
+          value={HIRAGANA}
+        />
+      </div>
+      <div class="f rc">
+        <label for={KATAKANA}>Katakana</label>
+        <input
+          name={KATAKANA}
+          type="radio"
+          bind:group={kata}
+          value={KATAKANA}
+        />
+      </div>
     </div>
+    <div class="f cc">
+      <h3>Charset</h3>
+      <Select options={charsetOptions} bind:selected={selectedCharsetIndex} />
+    </div>
+
+    <button class="big success mg" on:click={() => (gameStarted = true)}>
+      Start
+    </button>
+
+    <div class="f cc mg">
+      <h3>Game Options</h3>
+      <div class="f rc">
+        <label for="endlessMode">Endless Mode</label>
+        <input type="checkbox" name="endlessMode" bind:checked={endlessMode} />
+      </div>
+    </div>
+  </div>
 {/if}
