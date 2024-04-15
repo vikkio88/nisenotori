6805582628d1d10d69b4d21cc0b3936adc12d879
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-04-15 19:40:48 +0100

    format

--- src/pages/Dash.svelte
+++ src/pages/Dash.svelte
@@ -2,11 +2,12 @@
   import { navigate } from "svelte-routing";
 </script>
 
-<h1>Nise no tori</h1>
+<h1 title="偽の鳥">Nise no tori</h1>
+<h2 title="Fake Bird">偽の鳥</h2>
 
 <div class="f cc f1 g">
-  <button class="big" on:click={() => navigate("/cards", { replace: true })}
-    >Cards</button
-  >
+  <button class="big" on:click={() => navigate("/cards", { replace: true })}>
+    Cards
+  </button>
   <button class="big" disabled>Guess</button>
 </div>
