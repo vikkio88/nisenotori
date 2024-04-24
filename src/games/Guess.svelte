8a3dcf8cd742aa5d47da9b63b27e1e9a3eb7e06c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-04-24 20:43:00 +0100

    implemented audio guesser

--- src/games/Guess.svelte
+++ src/games/Guess.svelte
@@ -1,25 +1,85 @@
 <script>
+  // @ts-ignore
+  import { Howl } from "howler";
   import FlipCard from "../components/FlipCard.svelte";
   import { BASE } from "../libs/data/charSets";
   import { KATA_MAP } from "../libs/data/kataMap";
   import { GUESS_GAME_TYPES, getNewGuessingSet } from "./utils/guess";
+  import { audioUrlFromKata } from "../libs/audio";
+  import { HIRAGANA, KATAKANA } from "../libs/data/consts";
 
-  export let type = GUESS_GAME_TYPES.SYLLABLES;
+  export let kataChoice = HIRAGANA;
+  export let type = GUESS_GAME_TYPES.AUDIO;
   export let charset = BASE;
   export let options = 4;
-  
-  let { kata, possibleKatas } = getNewGuessingSet(charset, options);
+
+  let currentQuiz = getNewGuessingSet(charset, options, type);
+
+  let audio = null;
+  $: {
+    audio && audio.unload();
+
+    audio = new Howl({
+      src: audioUrlFromKata(currentQuiz.kata),
+    });
+    if (currentQuiz.guessType === GUESS_GAME_TYPES.AUDIO) {
+      // @ts-ignore
+      audio.once("load", () => audio.play());
+      // @ts-ignore
+      if (!audio.playing()) {
+        // @ts-ignore
+        audio.play();
+      }
+    }
+  }
+
+  function play() {
+    if (audio && !audio.playing()) {
+      audio.play();
+    }
+  }
+
+  function guess(romaji) {
+    if (currentQuiz.kata.romaji === romaji) {
+      console.log("CORRECT");
+    } else {
+      console.log("WRONG");
+    }
+
+    currentQuiz = getNewGuessingSet(charset, options, type);
+  }
 </script>
 
 <div class="f1 f cc">
-  {#if type === GUESS_GAME_TYPES.SYLLABLES}
-    <FlipCard {kata} hiragana />
+  {#if currentQuiz.guessType === GUESS_GAME_TYPES.SYLLABLES}
+    <FlipCard
+      kata={currentQuiz.kata}
+      hiragana={kataChoice === HIRAGANA}
+      katakana={kataChoice === KATAKANA}
+    />
+  {:else if currentQuiz.guessType === GUESS_GAME_TYPES.AUDIO}
+    <button class="huge success" on:click={play}>🔊</button>
   {/if}
-  <div class="fi mg g_5">
-    {#each possibleKatas as r}
-      <button class="big">
-        {KATA_MAP[r].romajiLabel || KATA_MAP[r].romaji}
+  <div class="fi mg2 g_5">
+    {#each currentQuiz.possibleKatas as r}
+      <button class="big" on:click={() => guess(r)}>
+        {#if currentQuiz.guessType === GUESS_GAME_TYPES.SYLLABLES}
+          {KATA_MAP[r].romajiLabel || KATA_MAP[r].romaji}
+        {:else if currentQuiz.guessType === GUESS_GAME_TYPES.AUDIO}
+          {#if kataChoice === HIRAGANA}
+            {KATA_MAP[r].hiragana}
+          {:else}
+            {KATA_MAP[r].katakana}
+          {/if}
+        {/if}
       </button>
     {/each}
   </div>
 </div>
+
+<style>
+  .huge {
+    font-size: 5rem;
+    padding: 4rem;
+  }
+</style>
--- src/games/utils/guess.js
+++ src/games/utils/guess.js
@@ -6,17 +6,30 @@ function byRandom() { return Math.random() - 0.5; }
 export const GUESS_GAME_TYPES = {
     SYLLABLES: 'syllables',
     AUDIO: 'audio',
-    COMBO_SYLLABLES: 'combo_syllables',
-    COMBO_AUDIO: 'combo_syllables',
-    COMBO: 'audio_syllables',
+    // COMBO_SYLLABLES: 'combo_syllables',
+    // COMBO_AUDIO: 'combo_syllables',
+    // COMBO: 'audio_syllables',
     COMBO_ALL: 'combo_all',
 };
 
-export function getNewGuessingSet(charset, options) {
+export const GUESS_TYPES_MAP = {
+    [GUESS_GAME_TYPES.COMBO_ALL]: [
+        GUESS_GAME_TYPES.SYLLABLES,
+        GUESS_GAME_TYPES.AUDIO,
+        // GUESS_GAME_TYPES.COMBO_SYLLABLES,
+        // GUESS_GAME_TYPES.COMBO_AUDIO
+    ],
+};
+
+export function getNewGuessingSet(charset, options, gameType) {
+    const guessType = (GUESS_TYPES_MAP[gameType] || [gameType]).sort(byRandom)[0];
+
+    //TODO add way to handle combo
     let kata = randomKata(charset);
     const chars = Object.values(ROMAJI_CHARS).filter(c => c != kata.romaji);
     chars.sort(byRandom);
     let possibleKatas = [...chars.slice(0, options - 1), kata.romaji];
     possibleKatas.sort(byRandom);
-    return { kata, possibleKatas };
+
+    return { kata, possibleKatas, guessType };
 }
