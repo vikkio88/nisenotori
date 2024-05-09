4393891dc672997f3c48f6170eac82685772d292
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-05-09 20:02:14 +0100

    made symbol also clickable

--- src/components/Symbol.svelte
+++ src/components/Symbol.svelte
@@ -1,46 +1,71 @@
 <script>
-    export let kata = null;
-    export let hiragana = false;
-    export let katakana = false;
-    export let romaji = false;
+  export let button = false;
+  export let kata = {};
+  export let hiragana = false;
+  export let katakana = false;
+  export let romaji = false;
 
-    export let small = false;
+  export let small = false;
 </script>
 
 {#if !Boolean(kata)}
-    <div />
-{:else}
-    <div class="symbol f cc" class:small>
-        {#if romaji}
-            <span>{kata.romajiLabel || kata.romaji}</span>
+  <div />
+{:else if button}
+  <button class="symbol ta-c" class:small on:click>
+    <div class="f cc">
+      {#if romaji}
+        <span>{kata.romajiLabel || kata.romaji}</span>
+      {/if}
+      <h1 class:small>
+        {#if hiragana}
+          {kata.hiragana}
+        {:else if katakana}
+          {kata.katakana}
+        {:else}
+          {kata.hiragana}
         {/if}
-        <h1 class:small>
-            {#if hiragana}
-                {kata.hiragana}
-            {:else if katakana}
-                {kata.katakana}
-            {:else}
-                {kata.hiragana}
-            {/if}
-        </h1>
+      </h1>
     </div>
+  </button>
+{:else}
+  <div class="symbol f cc" class:small>
+    {#if romaji}
+      <span>{kata.romajiLabel || kata.romaji}</span>
+    {/if}
+    <h1 class:small>
+      {#if hiragana}
+        {kata.hiragana}
+      {:else if katakana}
+        {kata.katakana}
+      {:else}
+        {kata.hiragana}
+      {/if}
+    </h1>
+  </div>
 {/if}
 
 <style>
-    div.symbol {
-        border: var(--borders);
-        border-radius: var(--border-radius);
-        padding: .5rem 1rem;
-    }
+  button.symbol {
+    all: unset;
+    border: var(--borders);
+    border-radius: var(--border-radius);
+    padding: 0.5rem 1rem;
+  }
 
-    h1 {
-        font-size: 2.5rem;
-    }
+  div.symbol {
+    border: var(--borders);
+    border-radius: var(--border-radius);
+    padding: 0.5rem 1rem;
+  }
 
-    h1.small {
-        font-size: 1.3rem;
-    }
-    div.small {
-        padding: 0.5rem 1rem;
-    }
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  h1.small {
+    font-size: 1.3rem;
+  }
+  div.small {
+    padding: 0.5rem 1rem;
+  }
 </style>
--- src/pages/Syllabary.svelte
+++ src/pages/Syllabary.svelte
@@ -1,172 +1,150 @@
 <script>
-    import { navigate } from "svelte-routing";
-    import Symbol from "../components/Symbol.svelte";
-    import SyllabarySelector from "../components/shared/SyllabarySelector.svelte";
-    import {
-        A,
-        K,
-        S,
-        T,
-        N,
-        H,
-        M,
-        Y,
-        R,
-        W,
-        n,
-        G,
-        Z,
-        D,
-        B,
-        P,
-    } from "../libs/data/charSets";
-    import { HIRAGANA, KATAKANA, ROMAJI } from "../libs/data/consts";
-    import { KATA_MAP } from "../libs/data/kataMap";
+  import { navigate } from "svelte-routing";
+  import Symbol from "../components/Symbol.svelte";
+  import SyllabarySelector from "../components/shared/SyllabarySelector.svelte";
+  import {
+    A,
+    K,
+    S,
+    T,
+    N,
+    H,
+    M,
+    Y,
+    R,
+    W,
+    n,
+    G,
+    Z,
+    D,
+    B,
+    P,
+  } from "../libs/data/charSets";
+  import { HIRAGANA, KATAKANA, ROMAJI } from "../libs/data/consts";
+  import { KATA_MAP } from "../libs/data/kataMap";
 
-    const baseRows = [
-        A,
-        K,
-        S,
-        T,
-        N,
-        H,
-        M,
-        [Y[0], null, Y[1], null, Y[2]],
-        R,
-        [W[0], null, null, null, W[1]],
-        [n, null, null, null, null],
-    ];
+  const baseRows = [
+    A,
+    K,
+    S,
+    T,
+    N,
+    H,
+    M,
+    [Y[0], null, Y[1], null, Y[2]],
+    R,
+    [W[0], null, null, null, W[1]],
+    [n, null, null, null, null],
+  ];
 
-    const dakuonRows = [G, Z, D, B, P];
+  const dakuonRows = [G, Z, D, B, P];
 
-    let baseRowsView = [...baseRows];
-    let dakuonRowsView = [...dakuonRows];
-    let filterTxt = "";
-    function onFilter() {
-        dakuonRowsView = [...dakuonRows];
-        baseRowsView = [...baseRows];
-        
-        if (Boolean(filterTxt)) {
-            // @ts-ignore
-            baseRowsView = baseRowsView.map((r) =>
-                r.filter((l) => l && l.includes(filterTxt.toLowerCase())),
-            );
-            dakuonRowsView = dakuonRowsView.map((r) =>
-                r.filter((l) => l && l.includes(filterTxt.toLowerCase())),
-            );
-        }
+  let baseRowsView = [...baseRows];
+  let dakuonRowsView = [...dakuonRows];
+  let filterTxt = "";
+  function onFilter() {
+    dakuonRowsView = [...dakuonRows];
+    baseRowsView = [...baseRows];
+
+    if (Boolean(filterTxt)) {
+      // @ts-ignore
+      baseRowsView = baseRowsView.map((r) =>
+        r.filter((l) => l && l.includes(filterTxt.toLowerCase()))
+      );
+      dakuonRowsView = dakuonRowsView.map((r) =>
+        r.filter((l) => l && l.includes(filterTxt.toLowerCase()))
+      );
     }
+  }
 
-    let romaji = true;
-    let kata = HIRAGANA;
+  let romaji = true;
+  let kata = HIRAGANA;
 </script>
 
 <h1>{kata}</h1>
 <div class="f r g">
-    <div class="f cc g_5 mg_5">
-        <SyllabarySelector bind:kata />
-    </div>
+  <div class="f cc g_5 mg_5">
+    <SyllabarySelector bind:kata />
+  </div>
 
-    <div class="f rc g_5 mg_5">
-        <label class="tt-cpz" for="romaji">{ROMAJI}</label>
-        <input type="checkbox" name="romaji" bind:checked={romaji} />
-    </div>
+  <div class="f rc g_5 mg_5">
+    <label class="tt-cpz" for="romaji">{ROMAJI}</label>
+    <input type="checkbox" name="romaji" bind:checked={romaji} />
+  </div>
 </div>
 
 <div class="f r">
-    <input
-        type="text"
-        bind:value={filterTxt}
-        on:input={onFilter}
-        placeholder="Filter syllables..."
-    />
-    <button
-        class="danger"
-        disabled={!Boolean(filterTxt)}
-        on:click={() => (filterTxt = "")}
-    >
-        Reset
-    </button>
+  <input
+    type="text"
+    bind:value={filterTxt}
+    on:input={onFilter}
+    placeholder="Filter syllables..."
+  />
+  <button
+    class="danger"
+    disabled={!Boolean(filterTxt)}
+    on:click={() => (filterTxt = "")}
+  >
+    Reset
+  </button>
 </div>
 
 <h2>Base</h2>
 {#each baseRowsView as row}
-    <div class="f r">
-        {#each row as syllable}
-            <div class="f1 f c">
-                {#if syllable}
-                    <div class="sWrapper">
-                        <Symbol
-                            hiragana={kata === HIRAGANA}
-                            katakana={kata === KATAKANA}
-                            kata={KATA_MAP[syllable]}
-                            {romaji}
-                        />
-                        <div class="btn f cc">
-                            <button
-                                class="small"
-                                on:click={() =>
-                                    navigate(
-                                        `/${kata}/${KATA_MAP[syllable].romaji}`,
-                                    )}
-                            >
-                                ➡️
-                            </button>
-                        </div>
-                    </div>
-                {:else}
-                    <div class="spacer" />
-                {/if}
-            </div>
-        {/each}
-    </div>
+  <div class="f r row">
+    {#each row as syllable}
+      <div class="f1 f c item">
+        {#if syllable}
+          <Symbol
+            button
+            on:click={() => navigate(`/${kata}/${KATA_MAP[syllable].romaji}`)}
+            hiragana={kata === HIRAGANA}
+            katakana={kata === KATAKANA}
+            kata={KATA_MAP[syllable]}
+            {romaji}
+          />
+        {:else}
+          <div class="spacer" />
+        {/if}
+      </div>
+    {/each}
+  </div>
 {/each}
 <h2>Dakuon / Handakuon</h2>
 {#each dakuonRowsView as row}
-    <div class="f r">
-        {#each row as syllable}
-            <div class="f1 f c">
-                <div class="sWrapper">
-                    <Symbol
-                        hiragana={kata === HIRAGANA}
-                        katakana={kata === KATAKANA}
-                        kata={KATA_MAP[syllable]}
-                        {romaji}
-                    />
-                    <div class="btn f cc">
-                        <button
-                            class="small"
-                            on:click={() =>
-                                //ADD Dakuon
-                                navigate(
-                                    `/${kata}/${KATA_MAP[syllable].romaji}`,
-                                )}
-                        >
-                            ➡️
-                        </button>
-                    </div>
-                </div>
-            </div>
-        {/each}
-    </div>
+  <div class="f r row">
+    {#each row as syllable}
+      <div class="f1 f c">
+        <Symbol
+          button
+          on:click={() => navigate(`/${kata}/${KATA_MAP[syllable].romaji}`)}
+          hiragana={kata === HIRAGANA}
+          katakana={kata === KATAKANA}
+          kata={KATA_MAP[syllable]}
+          {romaji}
+        />
+      </div>
+    {/each}
+  </div>
 {/each}
 
 <style>
-    h1 {
-        text-transform: capitalize;
-    }
-    h2 {
-        margin-bottom: 1rem;
-    }
-    .spacer {
-        padding: 2.3rem;
-    }
+  h1 {
+    text-transform: capitalize;
+  }
+  h2 {
+    margin-bottom: 1rem;
+  }
+  .spacer {
+    padding: 2.4rem;
+  }
 
-    .sWrapper > .btn {
-        visibility: hidden;
-    }
+  .row {
+    margin: 0.5rem 0.5rem;
+  }
 
-    .sWrapper:hover > .btn {
-        visibility: visible;
-    }
+  .item {
+    margin-left: 0.25rem;
+    margin-right: 0.25rem;
+  }
 </style>
