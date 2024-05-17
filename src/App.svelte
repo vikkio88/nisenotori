6b4804cb0a31ce13e1f4c8fe97c5d2ad3df90ae6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-05-17 19:26:57 +0100

    reorg

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import { Router, Route } from "svelte-routing";
   import Dash from "./pages/Dash.svelte";
-  import Cards from "./pages/Cards.svelte";
+  import Cards from "./pages/games/Cards.svelte";
   import Syllabary from "./pages/Syllabary.svelte";
   import Kata from "./pages/Kata.svelte";
   import { HIRAGANA, KATAKANA } from "./libs/data/consts";
-  import Guess from "./pages/Guess.svelte";
+  import Guess from "./pages/games/Guess.svelte";
   import Account from "./pages/Account.svelte";
 
   let url = "";
@@ -15,14 +15,16 @@
   <Router {url}>
     <Route path="/" component={Dash} />
     <Route path="/account" component={Account} />
+    
+    <!-- Learning -->
     <Route path="/syllabary" component={Syllabary} />
-
     <Route path="/hiragana/:romaji" let:params>
       <Kata romaji={params.romaji} kata={HIRAGANA} />
     </Route>
     <Route path="/katakana/:romaji" let:params>
       <Kata romaji={params.romaji} kata={KATAKANA} />
     </Route>
+    
     <!-- Games -->
     <Route path="/cards" component={Cards} />
     <Route path="/guess" component={Guess} />
